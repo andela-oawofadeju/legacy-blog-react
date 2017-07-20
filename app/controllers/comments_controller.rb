ede367ae24a726e3c09196c4b7310eff2d2893e6
I-Diva <olayemifolakemi@yahoo.com>
2017-07-20 05:36:58 +0100

    chore(controller): create controllers for all features

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,44 @@
 class CommentsController < ApplicationController
+  before_action: find_comment, only: [:edit, :show, :update, :delete]
+  
   def index
+  end
+
+  def comments
+    @comment = Comments.all
+  end
+
+  def new
+    @comments = Comments.new
+  end
+
+  def create
+    @comment = Comment.new
+    if @comment.save(comment_params)
+      flash[:notice] = "Successfully created comment"
+      redirect_to_path @comment
+    else
+      flash[:alert] = "Unable to create comment"
+      render :new
+  end
+
+  def edit
+  end
+
+  def destroy
+    if @comment.destroy(comment_params)
+      flash[:notice] = "Successfully deleted comment"
+      redirect_to_path
+    else
+      flash[:alert] = "Unable to delete comment"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:name, :comment)
+  end
+
+  def find_comment
+    @comment = Comment.find[params[:id]
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,45 @@
 class PostsController < ApplicationController
+  before_action: find_post, only[:edit, :show, :update, :delete]
+
+  #index acction to render all posts
   def index
+  end
+
+  def create
+    @post = Post.new
+    if @post.save(post_params)
+      flash[:notice] = "Successfully created a post"
+      redirect_to_post_path(@post)
+    else
+      flash[:alert] = "Error creating new post"
+      render :new
+  end
+  
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Successfully updated post"
+      redirect_to_post_path(@posts)
+    else
+      flash[:alert] = "could not update post"
+      render :edit
+  end
+
+  def delete
+    if @post.destroy(post_params)
+      flash[:notice] = "Successfully deleted posts"
+      redirect_to_post_path
+    else
+      flash[:alert] = "You are not allowed to deletethis post"
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
